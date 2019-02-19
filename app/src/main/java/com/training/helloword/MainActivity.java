3143582da08d1c1d243640b476d5cb96b7b469ad
Test <user.example.com>
2019-02-19 12:49:29 +0000

    Hide button when textview is clicked

--- app/src/main/java/com/training/helloword/MainActivity.java
+++ app/src/main/java/com/training/helloword/MainActivity.java
@@ -2,6 +2,9 @@ package com.training.helloword;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        final TextView textView = findViewById(R.id.hello_world_text);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setVisibility(View.GONE);
+            }
+        });
     }
 }
